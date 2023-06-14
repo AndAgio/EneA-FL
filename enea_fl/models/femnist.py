6b52e698e0483420bafc6ded8822476632eed28d
AndAgio <aagiollo@gmail.com>
2023-06-14 16:24:07 +0200

    chore: minors.

--- enea_fl/models/femnist.py
+++ enea_fl/models/femnist.py
@@ -6,9 +6,8 @@ from .config_files import FemnistConfig
 class CnnFemnist(nn.Module):
     def __init__(self, ):
         super(CnnFemnist, self).__init__()
-
         self.config = FemnistConfig()
-
+        # Convolution layers
         self.conv1 = nn.Conv2d(in_channels=self.config.image_shape[0],
                                out_channels=self.config.conv_channels[0],
                                kernel_size=self.config.conv_kernels[0],
@@ -17,6 +16,7 @@ class CnnFemnist(nn.Module):
                                out_channels=self.config.conv_channels[1],
                                kernel_size=self.config.conv_kernels[1],
                                stride=self.config.conv_strides[1])
+        # Fully connected layers
         self.fc1 = nn.Linear(in_features=self.config.lin_channels[0],
                              out_features=self.config.lin_channels[1])
         self.fc2 = nn.Linear(in_features=self.config.lin_channels[1],
--- enea_fl/models/sent140.py
+++ enea_fl/models/sent140.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 from .config_files import SentConfig
 import json
@@ -11,45 +10,37 @@ import torch.nn.functional as t_func
 class CnnSent(nn.Module):
     def __init__(self):
         super(CnnSent, self).__init__()
-
         self.config = SentConfig()
-
         try:
             with open(self.config.embs_file, 'r') as inf:
                 embs = json.load(inf)
         except FileNotFoundError:
-            rc = subprocess.call("./enea_fl/models/get_embs.sh", shell=True)
+            _ = subprocess.call("./enea_fl/models/get_embs.sh", shell=True)
             with open(self.config.embs_file, 'r') as inf:
                 embs = json.load(inf)
         vocab = embs['vocab']
         vocab_size = len(vocab)
         word_embeddings = torch.from_numpy(np.array(embs['emba']))
         word_embeddings_size = word_embeddings.shape[1]
-
         # Embedding Layer
         self.embeddings = nn.Embedding(vocab_size, word_embeddings_size)
         self.embeddings.weight = nn.Parameter(word_embeddings, requires_grad=False)
-
+        # Convolution Layers
         self.conv1 = nn.Conv1d(in_channels=word_embeddings_size, out_channels=self.config.num_channels,
                                kernel_size=self.config.kernel_size[0])
         self.conv2 = nn.Conv1d(in_channels=word_embeddings_size, out_channels=self.config.num_channels,
                                kernel_size=self.config.kernel_size[1])
         self.conv3 = nn.Conv1d(in_channels=word_embeddings_size, out_channels=self.config.num_channels,
                                kernel_size=self.config.kernel_size[2])
-
         self.dropout = nn.Dropout(self.config.dropout_keep)
-
-        # Fully-Connected Layer
+        # Fully connected layer
         self.fc = nn.Linear(self.config.num_channels * len(self.config.kernel_size), self.config.output_size)
-
-        # Softmax non-linearity
-        self.softmax = nn.Softmax()
 
     def forward(self, x):
         x = x.type(torch.LongTensor).permute(1, 0)
+        # Embed input to GloVe embeddings
         embedded_sent = self.embeddings(x)
         embedded_sent = embedded_sent.permute(1, 2, 0)
-
         # First convolution
         conv_out1 = t_func.relu(self.conv1(embedded_sent))
         kernel_size = self.config.max_sen_len - self.config.kernel_size[0] + 1
@@ -62,8 +53,8 @@ class CnnSent(nn.Module):
         conv_out3 = t_func.relu(self.conv3(embedded_sent))
         kernel_size = self.config.max_sen_len - self.config.kernel_size[2] + 1
         conv_out3 = t_func.max_pool1d(conv_out3, kernel_size=kernel_size).squeeze()
-
+        # Aggregate convolutions
         all_out = torch.cat((conv_out1, conv_out2, conv_out3), 1)
+        # Fully connected and output
         final_feature_map = self.dropout(all_out)
-
         return t_func.softmax(self.fc(final_feature_map), dim=1)
