f9c333a6051ea650b4037bdf2cf5d8cdfc55d800
AndAgio <aagiollo@gmail.com>
2023-06-16 17:37:02 +0200

    debug: gpu selection.

--- enea_fl/utils/gpu.py
+++ enea_fl/utils/gpu.py
@@ -1,27 +1,10 @@
-import subprocess
+import torch
+import numpy as np
 
 
-def run_cmd(cmd):
-    out = (subprocess.check_output(cmd, shell=True)).decode('utf-8')[:-1]
-    return out
-
-
-def get_free_gpu(logger):
-    out = run_cmd('nvidia-smi -q -d Memory | grep -A4 GPU')
-    out = (out.split('\n'))[1:]
-    out = [l for l in out if '--' not in l]
-
-    total_gpu_num = int(len(out)/5)
-    gpu_bus_ids = []
-    for i in range(total_gpu_num):
-        gpu_bus_ids.append([l.strip().split()[1] for l in out[i*5:i*5+1]][0])
-
-    out = run_cmd('nvidia-smi --query-compute-apps=gpu_bus_id --format=csv')
-    gpu_bus_ids_in_use = (out.split('\n'))[1:]
-    gpu_ids_in_use = []
-
-    for bus_id in gpu_bus_ids_in_use:
-        gpu_ids_in_use.append(gpu_bus_ids.index(bus_id))
-
-    logger.print_it([i for i in range(total_gpu_num) if i not in gpu_ids_in_use])
-    return [i for i in range(total_gpu_num) if i not in gpu_ids_in_use]
+def get_free_gpu():
+    free_memories = []
+    for i in range(torch.cuda.device_count()):
+        device = torch.device('cuda:{}'.format(i))
+        free_memories += torch.cuda.mem_get_info(device)
+    return np.argmax(np.asarray(free_memories))
