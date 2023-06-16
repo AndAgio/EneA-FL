ce3282d84e12189cc5fd56c3fabf73f5a7a0fe83
AndAgio <aagiollo@gmail.com>
2023-06-16 18:14:22 +0200

    debug: sent140 slow.

--- train_non_fl.py
+++ train_non_fl.py
@@ -162,15 +162,20 @@ class Trainer:
             labels = torch.from_numpy(np.array(batch_output)).to(self.processing_device)
             return inputs, labels
         elif self.dataset == 'sent140':
+            start = time.time()
             try:
                 _, indd, _ = get_word_emb_arr('enea_fl/models/embs.json')
             except FileNotFoundError:
                 _ = subprocess.call("./enea_fl/models/get_embs.sh", shell=True)
                 _, indd, _ = get_word_emb_arr('enea_fl/models/embs.json')
+            stop = time.time()
+            self.logger.print_it('Time taken to get indd: {:.3f}'.format(stop-start))
             x_batch = [e[4] for e in batch_input]
             x_batch = [line_to_indices(e, indd, max_words=SentConfig().max_sen_len) for e in x_batch]
             inputs = torch.from_numpy(np.array(x_batch)).type(torch.LongTensor).permute(1, 0).to(self.processing_device)
             labels = torch.from_numpy(np.array(batch_output)).to(self.processing_device)
+            stop = time.time()
+            self.logger.print_it('Time taken to preprocess sentence: {:.3f}'.format(stop-start))
             return inputs, labels
         else:
             raise ValueError('Dataset "{}" is not available!'.format(self.dataset))
