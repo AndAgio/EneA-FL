ef7df9e2149dd256919d189f9c19057b193d746e
AndAgio <aagiollo@gmail.com>
2023-06-17 11:05:15 +0200

    feat: LEAF model for femnist to check its performance.

--- enea_fl/models/config_files/femnist.py
+++ enea_fl/models/config_files/femnist.py
@@ -3,5 +3,5 @@ class Config(object):
     conv_channels = [32, 64]  # [20, 50]
     conv_kernels = [5, 5]  # [3, 3]
     conv_strides = [1, 1]
-    lin_channels = [7*7*64, 2048]  # [5*5*50, 500]
+    lin_channels = [4*4*64, 2048]  # [5*5*50, 500]
     n_classes = 62
