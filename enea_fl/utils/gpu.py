593a51cb65368ee65faff7f72b756b7cc17f9b23
Matteo Mendula <matteo.mendula@myndoor.it>
2023-06-23 21:02:39 -0700

    containers init

--- enea_fl/utils/gpu.py
+++ enea_fl/utils/gpu.py
@@ -1,10 +1,12 @@
 import torch
 import numpy as np
 
-
 def get_free_gpu():
     free_memories = []
     for i in range(torch.cuda.device_count()):
         device = torch.device('cuda:{}'.format(i))
-        free_memories += torch.cuda.mem_get_info(device)
+        # torch.cuda.mem_get_info
+        # Returns the global free and total GPU memory occupied for a given device using cudaMemGetInfo.
+        free_memory = torch.cuda.mem_get_info(device)[0]
+        free_memories += [free_memory]
     return np.argmax(np.asarray(free_memories))
